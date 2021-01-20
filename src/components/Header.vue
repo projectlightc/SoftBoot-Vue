d6bd389927a4b958f84102f499a3eb87ea5f1447
dn061198kav <anastasija.kuropjatnik@privatbank.ua>
2021-01-20 23:42:43 +0200

    did mobile version and fixed tablet version

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,19 +1,46 @@
 <template>
-  <header class="header">
-    <div class="haeder-section-container container-fluid">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-2 col-xs-2 logo"> лого </div>
-                <div class="col-md-7 col-xs-7">
-                    menu
-                </div>
-                <div class="col-md-3 col-xs-3">
-                    <button class="btn talk-button">Пообщаемся ?</button>
+    <header class="header">
+        <div class="haeder-section-container container-fluid">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-2 col-xs-2 logo">лого</div>
+                    <div class="col-md-9 col-xs-9 menu">
+                        <nav class="navbar navbar-expand-md">
+                            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
+                                <span class="navbar-toggler-icon"></span>
+                            </button>
+
+                            <div class="collapse navbar-collapse" id="navbarsExample04">
+                                <ul class="navbar-nav mr-auto">
+                                    <li class="nav-item active">
+                                        <a class="nav-link" href="#">Главная</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#">О нас</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#">Почему мы?</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#">Как работаем?</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#">Контакты</a>
+                                    </li>
+                                     <button class="btn talk-button">Пообщаемся ?</button>
+                                </ul>
+                            </div>
+                           
+                        </nav>
+                         
+                    </div>
+                    <!-- <div class="col-md-3 col-xs-3">
+                       
+                    </div> -->
                 </div>
             </div>
         </div>
-    </div>
-</header>
+    </header>
 </template>
 
 <script>
@@ -43,5 +70,22 @@ export default {
         background-color: #F5DF4D;
         border: 1px solid #F5DF4D;
         border-radius: 0;
+    }
+
+    /* .menu { 
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    } */
+
+    .nav-link {
+        font-size: 16px;
+        font-weight: 700;
+        line-height: 19px;
+        color: #2E3A59;
+    }
+
+    .navbar-expand-md .navbar-nav .nav-link {
+        padding-right: 1.87rem;
     }
 </style>
