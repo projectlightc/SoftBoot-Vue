6aef26a575201325629d6cb0390ceaed34dc63ab
dn061198kav <anastasija.kuropjatnik@privatbank.ua>
2021-01-28 23:11:21 +0200

    fixed locale button

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,101 +1,104 @@
 <template>
-    <header class="header sticky-top">
-        <div class="haeder-section-container container-fluid">
-            <div class="container">
-                <div class="row">
-                    <nav class="navbar navbar-expand-md">
-                        <div class="col-md-2 col-xs-2">
-                            <div class="navbar-block-button-logo">
-                                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
-                                    <span>
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-justify" viewBox="0 0 16 16">
-                                            <path
-                                                fill-rule="evenodd"
-                                                d="M2 12.5a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5z"
-                                            />
-                                        </svg>
-                                    </span>
-                                </button>
-                                <div class="logo">
-                                    <img src="../assets/images/softboot-logo.png" alt="softboot-logo" />
-                                </div>
+<header class="header sticky-top">
+    <div class="haeder-section-container container-fluid">
+        <div class="container">
+            <div class="row">
+                <nav class="navbar navbar-expand-md">
+                    <div class="col-md-2 col-xs-2">
+                        <div class="navbar-block-button-logo">
+                            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+                                <span>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-justify" viewBox="0 0 16 16">
+                                        <path
+                                            fill-rule="evenodd"
+                                            d="M2 12.5a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5z"
+                                        />
+                                    </svg>
+                                </span>
+                            </button>
+                            <div class="logo">
+                                <img src="../assets/images/softboot-logo.png" alt="softboot-logo" />
                             </div>
                         </div>
-                        <div class="col-md-10 col-xs-10 menu">
-                            <div class="collapse navbar-collapse" id="collapseExample">
-                                <ul class="navbar-nav mr-auto">
+                    </div>
+                    <div class="col-md-10 col-xs-10 menu">
+                        <div class="collapse navbar-collapse" id="collapseExample">
+                            <ul class="navbar-nav mr-auto">
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#item_1">{{ $t('li_item_1') }}</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#item_2">{{ $t('li_item_2') }}</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#item_3">{{ $t('li_item_3') }}</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#item_4">{{ $t('li_item_4') }}</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#item_5">{{ $t('li_item_5') }}</a>
+                                </li>
+                            </ul>
+
+                            <div class="locale_switcher">
+                                <ul class="nav nav-tabs" id="myTab" role="tablist">
                                     <li class="nav-item">
-                                        <a class="nav-link" href="#item_1">{{ $t('li_item_1') }}</a>
+                                        <span class="localeEng"></span><a class="nav-link eng active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true" @click="setLocale('en')">eng</a>
                                     </li>
                                     <li class="nav-item">
-                                        <a class="nav-link" href="#item_2">{{ $t('li_item_2') }}</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="#item_3">{{ $t('li_item_3') }}</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="#item_4">{{ $t('li_item_4') }}</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="#item_5">{{ $t('li_item_5') }}</a>
+                                        <span class="localeRu"></span><a class="nav-link ru" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false" @click="setLocale('ru')">ru</a>
                                     </li>
                                 </ul>
-                                
-                                <!-- <div class="locale_switcher">
-                                    <div class="en">
-                                        <span class="active"><a href="#" @click="setLocale('en')" class="nav-eng">eng</a></span>
-                                    </div>
-                                    <div class="ru">
-                                        <span><a href="#" @click="setLocale('ru')" class="nav-ru">ru</a></span>
-                                    </div>
-                                </div> -->
-                                <div class="site-nav">
-                                    <span class="localeEng" @click="setLocale('en')"><a href="#" >eng</a></span>
-                                    <span class="localeRu" @click="setLocale('ru')"><a href="#">ru</a></span>
-                                </div>
-                                <button type="button" class="btn talk-button" data-toggle="modal" data-target="#exampleModal">{{ $t('buttonText') }}</button>
                             </div>
+                            <button type="button" class="btn talk-button" data-toggle="modal" data-target="#exampleModal">{{ $t('buttonText') }}</button>
                         </div>
+                    </div>
 
-                        <!-- Modal -->
-                        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                            <div class="modal-dialog" role="document">
-                                <div class="modal-content">
-                                    <div class="modal-header">
-                                        <div class="modal-title">
-                                            <h5 id="exampleModalLabel">{{ $t('exampleModalLabel') }}<br>{{ $t('call') }}</h5>
-                                        </div>
-                        
-                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                            <span aria-hidden="true">&times;</span>
-                                        </button>
+                    <!-- Modal -->
+                    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <div class="modal-title">
+                                        <h5 id="exampleModalLabel">
+                                            {{ $t('exampleModalLabel') }}<br />
+                                            {{ $t('call') }}
+                                        </h5>
                                     </div>
-                                    <div class="modal-body">
-                                        <form action="#">
-                                            <div class="row">
-                                                <div class="form-group col-md-12 name">
-                                                    <input type="text" name="name" id="idName" :placeholder="$t('modal_name')">
-                                                </div>
-                                                <div class="form-group col-md-12 post">
-                                                    <input type="text" :placeholder="$t('modal_post')">
-                                                </div>
-                                                <div class="form-group col-md-12 phone">
-                                                    <input type="tel" :placeholder="$t('modal_phone')">
-                                                </div>
-                                                <div class="form-group col-md-12 button">
-                                                    <button class="btn btn-send">{{ $t('modal_btn') }}</button>
-                                                </div>
+
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <div class="modal-body">
+                                    <form action="#">
+                                        <div class="row">
+                                            <div class="form-group col-md-12 name">
+                                                <input type="text" name="name" id="idName" :placeholder="$t('modal_name')" />
                                             </div>
-                                        </form>
-                                    </div>
+                                            <div class="form-group col-md-12 post">
+                                                <input type="text" :placeholder="$t('modal_post')" />
+                                            </div>
+                                            <div class="form-group col-md-12 phone">
+                                                <input type="tel" :placeholder="$t('modal_phone')" />
+                                            </div>
+                                            <div class="form-group col-md-12 button">
+                                                <button class="btn btn-send">{{ $t('modal_btn') }}</button>
+                                            </div>
+                                        </div>
+                                    </form>
                                 </div>
                             </div>
                         </div>
-                    </nav>
-                </div>
+                    </div>
+                </nav>
             </div>
         </div>
-    </header>
+    </div>
+</header>
+
+    
 </template>
 
 
@@ -226,68 +229,29 @@ export default {
         margin-top: 66px;
     }
 
-    /* .locale_switcher {
-        width: 80px;
-        height: 26px;
-        display: flex;
-        justify-content: space-around;
-        margin-right: 60px;
-        border: 1px solid #cdcdcd;
+    .locale_switcher {
+        width: 82px;
+        height: 23px;
+        margin-right: 100px;
     }
 
-    .en span a { 
-        text-decoration: none;
-        color: #000;
+    .nav-link.active {
+        background-color: #F5DF4D !important;
     }
 
-    .ru span a {
-        text-decoration: none;
-        color: #000;
-    }
-
-    .nav-eng {
-        border-right: 1px solid #cdcdcd;
-    }
-
-    .nav-eng,
-    .nav-ru {
+    .nav-link.eng,
+    .nav-link.ru {
+        font-size: 14px;
+        font-weight: normal;
+        color: #000 !important;
         padding: 3px 10px;
+        border-radius: 0;
     }
 
-    .nav-eng:hover,
-    .nav-ru:hover {
-        background: #F5DF4D;
+    .nav-tabs {
+        border: none;
     }
 
-    span.active > a,
-    span.active > a:focus {
-        background: #F5DF4D;
-    }
-
-    .nav-eng:active,
-    .nav-ru:active {
-        background: #F5DF4D;
-        } */
-    /* .nav-link {
-        display: inline-block;
-        margin-right: 20px;
-        color: #000;
-        font-weight: 600;
-        font-size: 1.1em;
-        line-height: 1.3;
-        text-decoration: none;
-    }
-    .nav-link:hover, .nav-link:focus {
-        color: #be790d;
-        border-bottom: 10px solid rgba(190, 121, 13, 0.3);
-    }
-    .nav-link:active {
-        border-bottom: 20px solid rgba(190, 121, 13, 0.3);
-    } */
-
-    .is-active { 
-        color: red;
-    }
-
+    
 
 </style>
